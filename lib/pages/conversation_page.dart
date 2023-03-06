fe1ab85953357204622ded17549025a3dbac816d
duongquocanh1148 <duongquocanh1148@gmailcom>
2023-03-06 09:55:52 +0700

    desgin conversations

--- lib/pages/conversation_page.dart
+++ lib/pages/conversation_page.dart
@@ -1,5 +1,12 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:chatonline/models/conversation_models.dart';
 import 'package:chatonline/pages/add_conversation.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../widget/widgets.dart';
+
 
 class ConversationPage extends StatefulWidget {
   const ConversationPage({ Key? key }) : super(key: key);
@@ -25,7 +32,7 @@ class _ConversationPageState extends State<ConversationPage> {
           )
         ],
       ),
-      //body: ,
+      body: conversationList(),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
           Navigator.of(context).push(MaterialPageRoute(
@@ -34,6 +41,53 @@ class _ConversationPageState extends State<ConversationPage> {
         elevation: 0,
         child: const Icon(Icons.add),
       ),
+    );
+  }
+  conversationList(){
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid).collection('conversations')
+          .snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapShot) {
+        if (snapShot.hasData) {
+          return ListView.builder(
+            itemCount: snapShot.data!.docs.length,
+            itemBuilder: (context, index) {
+              ConversationModel conversationModel = ConversationModel.fromMap(snapShot.data!.docs[index].data() as Map<String, dynamic>);                  
+              return Container(
+                decoration: BoxDecoration(color: Colors.white, border: Border(bottom: BorderSide(color: Colors.grey.shade200))),
+                child: ListTile(              
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      leading: ClipOval(
+                            child: conversationModel.image!.isNotEmpty
+                                ? CachedNetworkImage(
+                                    imageUrl: conversationModel.image!,
+                                    width: 48,
+                                    height: 48,
+                                  )
+                                : Image.asset(
+                                    "assets/images/user_img.png",
+                                    width: 48,
+                                    height: 48,
+                                  ),
+                          ),
+                  title: Text(
+                    conversationModel.conName,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  onTap: () => {
+                    //nextScreenReplace(context, )
+                  },
+                ),
+              );
+            },
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
